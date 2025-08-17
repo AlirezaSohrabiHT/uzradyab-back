608e0e7cef67079fe8341244fb44ef446196bda9
am-pr <amirhossein.bohlour@gmail.com>
2025-08-17 16:21:23 +0330

    Update

--- managerPanel/settings.py
+++ managerPanel/settings.py
@@ -182,7 +182,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MERCHANT = "0a36c5ed-3774-4631-ada6-39b885d738a4"
 
-SANDBOX = True
+SANDBOX = False
 
 AUTH_USER_MODEL = 'accounts.User'
 
