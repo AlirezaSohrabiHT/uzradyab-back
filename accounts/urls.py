2144e7cbe973502b9674952f8aa3e75ceb247687
am-pr <amirhossein.bohlour@gmail.com>
2025-09-27 08:39:47 +0330

    add new endpoint for user_balance view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import LoginView , CheckTraccarTokenView , GenerateTraccarTokenView, user_info, edit_profile, change_password, reset_password
+from .views import LoginView , CheckTraccarTokenView , GenerateTraccarTokenView,\
+    user_info, edit_profile, change_password, reset_password, user_balance
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('check-traccar-token/', CheckTraccarTokenView.as_view(), name='check_traccar_token'),
     path('generate-traccar-token/', GenerateTraccarTokenView.as_view(), name='generate_traccar_token'),
     path('info/', user_info, name='user_info'),
+    path('balance/', user_balance, name='user_balance'),
     path('edit-profile/', edit_profile, name='edit_profile'),
     path('change-password/', change_password, name='change_password'),
     path('reset-password/', reset_password, name='reset_password'),
